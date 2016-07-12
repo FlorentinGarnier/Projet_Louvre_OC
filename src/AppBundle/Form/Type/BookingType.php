7c8526e55878b5a30161030cc127f8260e01ea1b
Florentin Garnier <garnier.florentin@gmail.com>
2016-07-12 23:14:19 +0200

    New logic

---
+++ src/AppBundle/Form/Type/BookingType.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Garnier
+ * Date: 12/07/2016
+ * Time: 23:05
+ */
+
+namespace AppBundle\Form\Type;
+
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class BookingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        
+    }
+}
